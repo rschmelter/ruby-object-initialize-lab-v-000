2bb1369ae0bf5f1b1d3bda53add93c1e1f471ef3
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-04 23:52:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog
   def initialize(name, breed="Mutt")
     @name = name
+    @breed = breed
   end
 
   def name=(name)
     @name = name
   end
+
+  def breed=(breed)
+    @breed = breed
   def name
     @name
   end
